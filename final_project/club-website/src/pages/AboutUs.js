bb58e5150c652aab8feb520b0caff492d11d5bdb
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2022-11-10 17:39:55 -0500

    finished draft of the about us and contact us page

--- final_project/club-website/src/pages/AboutUs.js
+++ final_project/club-website/src/pages/AboutUs.js
@@ -4,13 +4,28 @@ function AboutUs() {
   return (
     <>
       <div className="about-us">
-        <h1>About Us</h1>
-        <p>
-          Our club is a group of students who are interested in learning about
-          the world of technology. We meet every week to discuss the latest tech
-          news and to learn about new technologies. We also have a lot of fun
-          doing it!
-        </p>
+        <div className="about-us-title" >
+          <h1>WHO ARE WE?</h1>
+          <h2>WHO ARE YOU? THAT'S WHAT WE WANNA KNOW</h2>
+        </div>
+        <div className="about-us-info">
+          <p>
+          Queen's People Watching Club is a non-profit organization that 
+          watches targetted individuals for meticulous reasons that are 
+          confidential to only the watcher themselves.
+          </p>
+          <br/>
+          <p>
+          Our mission is to enhance our observation senses by following 
+          the routines of handpicked individuals that catches our eye. 
+          As you join us in our rituals, you shall open your mind more 
+          to more than one world, through the lives of other fellow 
+          human beings that are minding their own business. 
+          </p>
+          <a href="/">
+            <button className="register-button">Register Now!</button>
+          </a>
+        </div>
       </div>
     </>
   );
